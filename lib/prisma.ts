1c0e7bace72f044a5f67b3f8577746e3c67daad8
Clément Subtil <clementsubtil2006@gmail.com>
2026-04-29 18:58:12 +0000

    fix: use require for prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,11 @@
-// @ts-expect-error - Prisma client dynamic import
+// Prisma client using require
+/* eslint-disable @typescript-eslint/no-var-requires */
 let prismaClient: any;
 
 export async function prisma() {
   if (!prismaClient) {
-    const pc = await import('@prisma/client');
-    const PrismaClient = (pc as any).default?.PrismaClient || (pc as any).PrismaClient;
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { PrismaClient } = require('@prisma/client');
     prismaClient = new PrismaClient();
   }
   return prismaClient;
